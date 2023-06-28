5974a0b798761a57eb2193b22341562b55411c5b
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-06-28 18:52:15 +0700

    update location

---
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quanlychitieu.R;
+
+public class MainActivity extends  AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        FragmentItemAdd fragmentItemAdd = new FragmentItemAdd();
+//
+//        // Replace the fragmentContainer with the FragmentTest fragment
+//        getSupportFragmentManager().beginTransaction()
+//                .replace(R.id.fragmentContainerView4, fragmentItemAdd)
+//                .commit();
+
+    }
+}
